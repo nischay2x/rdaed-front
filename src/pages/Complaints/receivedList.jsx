e30143a1412a22dcced0e9abc67dc9e78816e3b5
Nischay Chandra <77281955+nischay2x@users.noreply.github.com>
2022-12-04 23:38:38 +0530

    docuemnts and complaints with build

--- src/pages/Complaints/receivedList.jsx
+++ src/pages/Complaints/receivedList.jsx
@@ -1,54 +1,102 @@
 import { Grid, Paper, Box, Typography, Button } from "@mui/material";
 import BgImage from "../../components/bgImage";
 import { user } from "../../config/constants";
+import axios from "axios";
+import { baseUrl } from "../../config/api-config.js";
+import { useEffect } from "react";
+import { useUserContext } from "../../components/UserContext.js";
+import { useState } from "react";
+
+async function getComplaints (token) {
+    try {
+        const { data } = await axios.get(`${baseUrl}/employee_portal/get/complaints/`, {
+            headers: {
+                authorization: 'Bearer '+token
+            }
+        });
+        return { data, error: false }
+    } catch (error) {
+        return { error }
+    }
+}
+
 
 export default function ReceivedList() {
+
+    const userContext = useUserContext();
+    const userData = userContext.useUser();
+    const [complaints, setComplaints] = useState([]);
+
+    useEffect(() => {
+        if(!userData.token) return;
+        getComplaints(userData.token).then((res) => {
+            if(res.error) {
+                alert('Error While Fetching Complaints.')
+            } else {
+                setComplaints(res.data);
+            }
+        })
+    }, [userData.token])
+
     return (
         <Grid container spacing={2}>
+            {
+                complaints.map((c, i) => <ComplaintCard  data={c} key={i} />
+                )
+            }
+            {/* <ComplaintCard/>
             <ComplaintCard/>
             <ComplaintCard/>
             <ComplaintCard/>
-            <ComplaintCard/>
-            <ComplaintCard/>
+            <ComplaintCard/> */}
         </Grid>
     )
 }
 
-function ComplaintCard() {
+[{
+    "id":1,
+    "username":"abhi1820",
+    "name":"Abhishek Soni",
+    "complaint_id":"abhi182051",
+    "subject":"something",
+    "file":"/rdamedia/rdamedia/complaint/product-jpeg-500x500.jpeg",
+    "description":"kuch toh hua hai",
+    "date":"2022-12-04T12:53:11.424750Z",
+    "status":"Waiting"
+}]
+
+function ComplaintCard({data}) {
     return (
         <Grid item xs={12} sm={10} md={6} lg={5} xl={4}>
             <Box sx={{ borderRadius: '25px', backgroundColor: '#fff' }}>
                 <Box display='flex' alignItems='center' columnGap={1} p={2}  >
                     <BgImage src={user.image} width="40px" height="40px" borderRadius="50%" />
                     <Box>
-                        <Typography>{user.name}</Typography>
-                        <Typography fontSize='small' color='gray'>22 Sept 2022, 09:30 PM</Typography>
+                        <Typography>{data.name}</Typography>
+                        <Typography fontSize='small' color='gray'>{new Date(data.date).toLocaleString()}</Typography>
                     </Box>
                 </Box>
                 <Box px={2} pb={2}>
                     <Typography pb={1} fontWeight='500'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, doloribus!
+                        {data.subject}
                     </Typography>
 
                     <Typography fontSize='small' sx={{ lineHeight: '2ch' }}>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Molestiae, temporibus facere! Eveniet voluptas ipsum
-                        illum dolore hic pariatur minima odit asperiores sit
-                        doloribus labore nemo, enim maiores cupiditate porro quia!
+                        {data.description}
                     </Typography>
                 </Box>
-                <Box pb={2} px={2}>
+                <Box pb={2} px={2} textAlign='center'>
                     <Button color="customBlue"
-                        fullWidth
                         sx={{
+                            minWidth: '150px',
                             boxShadow: '0px 0px 63px rgba(39, 39, 39, 0.15)',
                             color: '#fff',
                             textTransform: 'none',
                             borderRadius: '25px'
-                        }} variant="contained">Resolved</Button>
+                        }} variant="contained">Resolve</Button>
                 </Box>
                 <Box textAlign='center' sx={{ backgroundColor: '#ededed', py: '4px' }}>
-                    <Typography fontSize='small' color='#6c6c6c'>Complaint Number: 09</Typography>
+                    <Typography fontSize='small' color='#6c6c6c'>Complaint Number: {data.id}</Typography>
                 </Box>
             </Box>
         </Grid>
--- src/pages/Docs/list.jsx
+++ src/pages/Docs/list.jsx
@@ -1,20 +1,65 @@
-import { Grid } from "@mui/material"
+import { Grid, Paper, Typography, Button } from "@mui/material"
+import axios from "axios";
 import DocumentCard from "./DocumentCard";
+import { baseUrl } from "../../config/api-config.js";
+import { useEffect } from "react";
+import { useUserContext } from "../../components/UserContext.js";
+import { useState } from "react";
+import { Box } from "@mui/system";
+
+
+
+async function getDocuments (token) {
+    try {
+        const { data } = await axios.get(`${baseUrl}/employee_portal/get/documents/`, {
+            headers: {
+                authorization: 'Bearer '+token
+            }
+        });
+        return { data, error: false }
+    } catch (error) {
+        return { error }
+    }
+}
+
 
 export default function DocsList() {
 
+    const userContext = useUserContext();
+    const userData = userContext.useUser();
+    const [docs, setDocs] = useState([]);
+
+    useEffect(() => {
+        if(!userData.token) return;
+        getDocuments(userData.token).then((res) => {
+            if(res.error) {
+                alert('Error While Fetching Documents.')
+            } else {
+                setDocs(res.data);
+            }
+        })
+    }, [userData.token])
+
     return <Grid container spacing={2}>
-        <Grid item xs={12} sm={10} md={6} lg={4} xl={3}>
-            <DocumentCard />
-        </Grid>
-        <Grid item xs={12} sm={10} md={6} lg={4} xl={3}>
-            <DocumentCard />
-        </Grid>
-        <Grid item xs={12} sm={10} md={6} lg={4} xl={3}>
-            <DocumentCard />
-        </Grid>
-        <Grid item xs={12} sm={10} md={6} lg={4} xl={3}>
-            <DocumentCard />
-        </Grid>
+        {
+            docs.map((d, i) => <Grid key={i} item xs={12} sm={10} md={6} lg={4} xl={3}>
+                <DocCard data={d} />
+            </Grid>)
+        }
     </Grid>
+}
+
+function DocCard ({ data }) {
+    return (
+        <Paper sx={{ p: 1 }} elevation={2}>
+            <Typography><b>Filename :</b> {data.name}</Typography>
+            <Typography><b>Type :</b> {data.doc_type}</Typography>
+            <Typography><b>Status :</b> {data.status}</Typography>
+            <a href={baseUrl+data.file} target='_blank'>Click To View</a>
+            <Box py={1} display='flex' justifyContent='center' columnGap={1}>
+                <Button variant="contained" color="primary" size='small'>Accept</Button>
+                <Button variant="contained" color="warning" size='small'>Reject</Button>
+            </Box>
+        </Paper>
+    )
 }
